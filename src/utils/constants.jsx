5a4e400084833f70ec9a0474ce8d09402aaaca66
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-10-01 16:41:17 +0530

    Updated constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,4 +5,4 @@ export const CDN_IMG_URL_BASE = "https://media-assets.swiggy.com/swiggy/image/up
 
 export const LOGO_URL = "https://res.cloudinary.com/de6mfpqpf/image/upload/v1723029159/Resto_logo_g1xepa.png"
 
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
+export const MENU_URL = "https://bitefinder-server.onrender.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
